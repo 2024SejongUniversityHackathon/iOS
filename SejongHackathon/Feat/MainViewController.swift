d423d403cd6beec9163b5fc95065c444f499b24f
jeoungsung12 <jeoungsung12@naver.com>
2024-05-17 17:21:12 +0900

    init setting

---
+++ SejongHackathon/Feat/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SejongHackathon
+//
+//  Created by 정성윤 on 2024/05/17.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
